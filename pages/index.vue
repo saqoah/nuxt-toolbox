506dd0483c7b43bbcfdce9e4461dd0951ba75ad6
hmz <66381391+saqoah@users.noreply.github.com>
2022-06-26 03:54:51 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <main class="intro1">
-    <div class="intro">Hi 👋!have fun.</div>
+  <main class="intro">
     <FeedbackForm />
-    <JokeBlock />
   </main>
 </template>
 
@@ -10,11 +8,8 @@
 * {
   font-family: 'Helvetica', sans-serif;
 }
+
 .intro {
-  background: #494646;
-  min-width: 400px;
-}
-.intro1 {
   background: #494646;
 }
 </style>
