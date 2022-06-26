bd6df0bd1f488332fa1fc4e94a423f02107f977f
hmz <66381391+saqoah@users.noreply.github.com>
2022-06-26 03:36:19 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,11 @@
 <template>
   <main>
     <div class="intro">
-      <h1>Nuxt Toolbox</h1>
+      <h1>Youtube - CookieViews</h1>
       <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
+        Hi 👋! This bot gives you unlimited views on youtube shorts,
+        the bot you would need to hit the max of your channel,
         with a few fun features.
-      </p>
-
-      <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
       </p>
     </div>
     <FeedbackForm />
